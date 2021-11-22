5333fdc0a1b0fdab8bc2fa46fda62ce27163bec4
ITITIU20292 <ITITIU20292@student.hcmiu.edu.vn>
2021-11-22 19:10:37 +0700

    update ex5

--- src/Exercise5.c
+++ src/Exercise5.c
@@ -17,43 +17,18 @@ Ex:
 
 int Ex5(char *str){
 	//Your codes here
-	int size=strlen(str);
-	char buf[size];
-	int i=0;
-   while(*str)
-    {
-        if(*str == '(' || *str == '[' || *str == '{')
-        {
-            buf[i] = *str;
-            i++;
-        }
-        if(*str == ')')
-        {
-            if(buf[i - 1] == '(')
-                i--;
-            else
-                return 0;
-        }
-        if(*str == ']')
-        {
-            if(buf[i - 1] == '[')
-                i--;
-            else
-                return 0;
-        }
-        if(*str == '}')
-        {
-            if(buf[i - 1] == '{')
-                i--;
-            else
-                return 0;
-        }
-        str++;
-    }
-    if(i == 0)
-        return 1;
-    else
-        return 0;
+   int Ex5 = 1;
+   for(int i = 0; i < strlen(str); i++ ) {
+
+   if((str[i] == '(' && str[i+1] ==')') || 
+       (str[i] == '{' && str[i+1] =='}') || 
+       (str[i] == '[' && str[i+1] ==']'))
+     continue;
+
+    Ex5= 0;
+    break;
+   }
+ return Ex5;
 }
 	
 
@@ -62,10 +37,12 @@ int main(int argc, char *argv[]) {
 	//testing variable, applying it to your algorithm for auto-evaluating
 	char *testcase = argv[1];
 
-	if(Ex5(testcase)==1)
+	if(Ex5(testcase)==0)
 	{
 		printf("Valid");
 	}else
+	{
 	printf("Invalid");
+	}
 	return 0;
 }
